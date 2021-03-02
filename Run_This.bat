efe7ee1f39deac617305a4dd154bd79aed06779b
DerrikCreates <heyimderrik@gmail.com>
2021-03-02 03:16:58 -0500

    Added feed back and added a more naive logging
    Remove the code i didnt understand for a more naive approach

--- Run_This.bat
+++ Run_This.bat
@@ -1,17 +1,26 @@
 @echo WAIT FOR THIS WINDOW TO AUTOMATICALLY CLOSE (1-2m)
 @echo off
-@echo Enter IP/Domain
-set /p ip=""
+@echo Enter IP/Domain and press enter
+@set /p ip=""
+@echo Starting Test with address of %ip%
 set LOGFILE=ConnectionTestOutput.log
-call :LOG > %LOGFILE%
-exit /B
 
-:LOG
-echo Starting Test
-echo Username: %username% 
-tracert %ip%
-@echo normal iperf3 Test
-.\iperf3.exe -c %ip%
-@echo Reverse iperf3 Test
-.\iperf3.exe -c %ip%
-.\speedtest.exe --accept-license
+
+
+@echo Starting Test >> LOGFILE
+@echo Username: %username% >> LOGFILE
+@echo Starting Traceroute...
+tracert %ip% >> LOGFILE
+@echo Traceroute Done
+@echo Starting two way test with iperf3...
+@echo normal iperf3 Test >> LOGFILE
+.\iperf3.exe -c %ip% >> LOGFILE
+@echo Reverse iperf3 Test >> LOGFILE
+.\iperf3.exe -c %ip% >> LOGFILE
+@echo iperf3 tests done
+@echo Starting speed test...
+.\speedtest.exe --accept-license >> LOGFILE
+
+
+set /p empty=Your test is done! File saved to LOGFILE.log
+set /p Press enter to close
