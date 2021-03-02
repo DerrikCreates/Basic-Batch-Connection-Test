79c79761dd38c2b1cd25557b1b0ff333a4d71639
DerrikCreates <heyimderrik@gmail.com>
2021-03-02 02:44:32 -0500

    Added user input for ip

--- Run_This.bat
+++ Run_This.bat
@@ -1,15 +1,17 @@
 @echo WAIT FOR THIS WINDOW TO AUTOMATICALLY CLOSE (1-2m)
 @echo off
+@echo Enter IP/Domain
+set /p ip=""
 set LOGFILE=ConnectionTestOutput.log
 call :LOG > %LOGFILE%
 exit /B
 
 :LOG
 echo Starting Test
-echo %username%
-tracert DOMAIN
+echo Username: %username% 
+tracert %ip%
 @echo normal iperf3 Test
-.\iperf3.exe -c DOMAIN
+.\iperf3.exe -c %ip%
 @echo Reverse iperf3 Test
-.\iperf3.exe -c DOMAIN
+.\iperf3.exe -c %ip%
 .\speedtest.exe --accept-license
